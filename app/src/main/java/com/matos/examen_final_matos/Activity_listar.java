e8d1d99fab0527d0404f385af2b1d52fe19b500a
Daniel Matos <deme12.1@gmail.com>
2023-10-02 20:04:05 -0500

    Conectar a Firebase

--- app/src/main/java/com/matos/examen_final_matos/Activity_listar.java
+++ app/src/main/java/com/matos/examen_final_matos/Activity_listar.java
@@ -3,12 +3,42 @@ package com.matos.examen_final_matos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.matos.examen_final_matos.model.Lugar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Activity_listar extends AppCompatActivity {
+
+    FloatingActionButton btnAgregar;
+    ListView lstLugares;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    List<Lugar> listaLugares = new ArrayList<>();
+    ArrayAdapter<Lugar> adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar);
+
+        asignarReferencias();
+        inicializarFirebase();
+        listarDatos();
+    }
+
+    private void listarDatos() {
+    }
+
+    private void inicializarFirebase() {
+    }
+
+    private void asignarReferencias() {
     }
 }
