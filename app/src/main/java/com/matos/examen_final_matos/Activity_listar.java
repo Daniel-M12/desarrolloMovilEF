17a9951923b40e65204f8afc2028ce41684cafec
Daniel Matos <deme12.1@gmail.com>
2023-10-02 20:43:51 -0500

    Funcionalidad de Registrar y Listar

--- app/src/main/java/com/matos/examen_final_matos/Activity_listar.java
+++ app/src/main/java/com/matos/examen_final_matos/Activity_listar.java
@@ -1,14 +1,24 @@
 package com.matos.examen_final_matos;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.matos.examen_final_matos.model.Lugar;
 
 import java.util.ArrayList;
@@ -34,11 +44,60 @@ public class Activity_listar extends AppCompatActivity {
     }
 
     private void listarDatos() {
+        reference.child("Lugar").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listaLugares.clear();
+                for (DataSnapshot item:snapshot.getChildren()) {
+                    Lugar lugar = item.getValue(Lugar.class);
+                    listaLugares.add(lugar);
+                }
+                adaptador = new ArrayAdapter<Lugar>(Activity_listar.this,
+                        android.R.layout.simple_list_item_1,
+                        listaLugares);
+                lstLugares.setAdapter(adaptador);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
     }
 
     private void asignarReferencias() {
+        btnAgregar = findViewById(R.id.btnAgregar);
+        btnAgregar.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+        lstLugares = findViewById(R.id.lstLugares);
+        lstLugares.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(Activity_listar.this,
+                        "" + listaLugares.get(i).getLatitud(),
+                        Toast.LENGTH_LONG);
+
+                //mostrarMensaje(listaLugares.get(i).getLatitud());
+            }
+        });
+    }
+
+    private void mostrarMensaje(String mensaje) {
+        AlertDialog.Builder ventana = new AlertDialog.Builder(this);
+        ventana.setTitle("Mensaje inormativo");
+        ventana.setMessage(mensaje);
+        ventana.setPositiveButton("Aceptar", (dialogInterface, i) -> {
+            Intent intent = new Intent(this, Activity_listar.class);
+            startActivity(intent);
+        });
+        ventana.create().show();
     }
 }
--- app/src/main/java/com/matos/examen_final_matos/MainActivity.java
+++ app/src/main/java/com/matos/examen_final_matos/MainActivity.java
@@ -1,15 +1,26 @@
 package com.matos.examen_final_matos;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.matos.examen_final_matos.model.Lugar;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText txtNombreLugar, txtLatitud, txtLongitud;
     Button btnRegistrar;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         txtNombreLugar = findViewById(R.id.txtNombreLugar);
         txtLatitud = findViewById(R.id.txtLatitud);
         txtLongitud = findViewById(R.id.txtLongitud);
+        btnRegistrar = findViewById(R.id.btnRegistrar);
 
         btnRegistrar.setOnClickListener(v -> {
             registrar();
@@ -31,9 +43,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void registrar() {
+        String nombreLugar = txtNombreLugar.getText().toString();
+        String latitud = txtLatitud.getText().toString();
+        String longitud = txtLongitud.getText().toString();
 
+        Lugar lugar = new Lugar();
+
+        lugar.setId(UUID.randomUUID().toString());
+        lugar.setNombreLugar(nombreLugar);
+        lugar.setLatitud(latitud);
+        lugar.setLongitud(longitud);
+
+        reference.child("Lugar")
+                .child(lugar.getId())
+                .setValue(lugar);
+
+        mostrarMensaje("Lugar Registrado");
+    }
+
+    private void mostrarMensaje(String mensaje) {
+        AlertDialog.Builder ventana = new AlertDialog.Builder(this);
+        ventana.setTitle("Mensaje inormativo");
+        ventana.setMessage(mensaje);
+        ventana.setPositiveButton("Aceptar", (dialogInterface, i) -> {
+            Intent intent = new Intent(this, Activity_listar.class);
+            startActivity(intent);
+        });
+        ventana.create().show();
     }
 
     private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
     }
 }
